7237bb83b2dfc2fc3b0f6a5307c167dc5d2a4a91
nachool <nachma.olivia1394@gmail.com>
2025-11-07 19:11:59 +0700

    menambah spacer

--- app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
@@ -47,8 +47,8 @@ fun TampilData(
                         Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
                     }
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
-
                 }
+                Spacer(modifier = Modifier.height(10.dp))
             }
         }
 
