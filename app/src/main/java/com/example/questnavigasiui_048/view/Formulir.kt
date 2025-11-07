a32d1fd8ac7078e057a2c2723abecb1162f7a636
nachool <nachma.olivia1394@gmail.com>
2025-11-07 17:50:25 +0700

    menambah function FormIsian dan menambah variabel

--- app/src/main/java/com/example/questnavigasiui_048/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_048/view/Formulir.kt
@@ -1,2 +1,12 @@
 package com.example.questnavigasiui_048.view
 
+import android.R
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+) {
+
+}
