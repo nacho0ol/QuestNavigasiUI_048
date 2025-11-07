b8e547d342d8dfb959b453764d24ac5099e8ba76
nachool <nachma.olivia1394@gmail.com>
2025-11-07 18:41:04 +0700

    menambahkan variabel items

--- app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
@@ -1,9 +1,18 @@
 package com.example.questnavigasiui_048.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_048.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnCLick: () -> Unit
-)
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Yang Lain"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
+}
