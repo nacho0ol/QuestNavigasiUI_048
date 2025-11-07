88823fe73a977bcac37c840634654f9a3a096671
nachool <nachma.olivia1394@gmail.com>
2025-11-07 18:38:53 +0700

    menambahkan function TampilData dan OptIn

--- app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
@@ -1,2 +1,9 @@
 package com.example.questnavigasiui_048.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnCLick: () -> Unit
+)
