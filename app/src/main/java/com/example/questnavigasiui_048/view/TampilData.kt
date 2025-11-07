00e75490b802dea0bb80223af362d06efcd9de6c
nachool <nachma.olivia1394@gmail.com>
2025-11-07 19:05:16 +0700

    menambah topappbar

--- app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_048/view/TampilData.kt
@@ -2,9 +2,13 @@ package com.example.questnavigasiui_048.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.questnavigasiui_048.R
 
